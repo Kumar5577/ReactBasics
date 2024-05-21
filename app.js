975840b3874b86faed76dc0fa22957c4e0d9067d
Kumar <satishkumar259731@gmail.com>
2024-05-22 00:10:51 +0530

    React-2

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
 const heading = React.createElement(
         "div",
         {id : "heading"},
         [React.createElement("div",{id:"child-1",key:"child-1"}),[
-                React.createElement("h2",{key : "1"},"I'm from heading2"),
-        React.createElement("p",{key : "2"},"I'm heading2")],
+                React.createElement("h2",{key : "1"},"I'm child-1"),
+        React.createElement("p",{key : "2"},"I'm also from chld-1")],
         React.createElement("div",{id:"child-2",key:"child-2"}),[
-                React.createElement("h2",{key : "3"},"I'm from child2"),
+                React.createElement("h2",{key : "3"},"I'm  "),
         React.createElement("p",{key : "4"},"I'm child2")]]
         );
       
