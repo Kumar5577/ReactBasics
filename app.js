405af9853d0d1b85ec3aefd0895844b0e2aab2e7
Kumar <satishkumar259731@gmail.com>
2024-05-22 16:01:17 +0530

    React-3

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-const heading = React.createElement(
-        "div",
-        {id : "heading"},
-        [React.createElement("div",{id:"child-1",key:"child-1"}),[
-                React.createElement("h2",{key : "1"},"I'm child-1"),
-        React.createElement("p",{key : "2"},"I'm also from chld-1")],
-        React.createElement("div",{id:"child-2",key:"child-2"}),[
-                React.createElement("h2",{key : "3"},"I'm  "),
-        React.createElement("p",{key : "4"},"I'm child2")]]
+// React Element
+const heading = <h1 id="heading">This is from Jsx</h1>
+//React Component
+        const headingComponent = (
+                <h1>This is React Element</h1>
         );
-      
       const root = ReactDOM.createRoot(document.getElementById("root"));
        console.log("hi");
-      root.render(heading);
+       const Title = () => <h1>This is React Component</h1>;
+       const HeadingComponent2 = () => (
+        <div><p>This is React Component</p>
+        {headingComponent}
+        {Title()}
+        </div>
+        );
+      root.render(<HeadingComponent2/>);
      
      
